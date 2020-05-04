6792f7a4f0c94dd49e9c6f312941a9aa489c529b
Sergo <46654316+s3rxio@users.noreply.github.com>
2020-05-04 21:35:11 +0300

    Add files via upload

--- SCPDiscordBotOnline/index.js
+++ SCPDiscordBotOnline/index.js
@@ -8,6 +8,7 @@ let fetch = require("node-fetch");
 console.log("Modules Loaded!".green);
 
 let cfg = require("./cfg.json");
+
 console.log("Config Loaded!".green);
 console.log("========End========".bgRed);
 
@@ -15,20 +16,34 @@ let clientConsole = {
     log: async (client, ...data) => {
         console.log(`[${client.user.id} | ${client.user.username}]`.magenta + `${data}`);
     }
-}
+};
 
 global.servers = {};
-
 
 let ports = cfg.PORTS;
 let tokens = cfg.tokens;
 
 if(ports.length != tokens.length) {
     for(i = 0; i < 15; i++) {
-        console.log(`Port length dont equal tokens length`.red)
-    }
+        console.log(`Port length dont equal tokens length`.red);
+    };
     return process.exit();
-}
+} else if (!cfg.ApiKey) {
+    for (i = 0; i < 15; i++) {
+        console.log(`Paste ApiKey in cfg.json`.red);
+    };
+    return process.exit();
+} else if (!cfg.AccountID) {
+    for (i = 0; i < 15; i++) {
+        console.log(`Paste AccountID in cfg.json`.red);
+    };
+    return process.exit();
+} else if (!cfg.IP) {
+    for (i = 0; i < 15; i++) {
+        console.log(`Paste IP in cfg.json`.red);
+    };
+    return process.exit();
+};
 
 let clients = {};
 
@@ -37,22 +52,29 @@ ports.forEach((port, i) => {
     var client = new Client();
     clients[port] = client;
     client.login(tokens[i]);
-
+    
     client.on('ready', async () => {
         clientConsole.log(client, ` Logged as `.cyan+`${client.user.tag}`.green);
         setInterval(() => {
             if(servers[port]) {
-            let cache = JSON.parse(fs.readFileSync('./cache.json'));
-            data = [];
-            data[2] = cache[port];
-            data[0] = data[2]['Players'].split('/')[0];
-            data[1] = data[2]['Players'].split('/')[1];
+                let cache = JSON.parse(fs.readFileSync('./cache.json'));
+                data = [];
+                data[2] = cache[port];
+                if(data[2]['Players']) {
+                    data[0] = data[2]['Players'].split('/')[0];
+                    data[1] = data[2]['Players'].split('/')[1];
+                } else {
+                    
+                    for (i = 0; i < 15; i++) {
+                        console.log("Server on? Players undefined!!!!!!!".red);
+                };
+            };
 
             if (typeof data === 'string') {
                 client.user.setActivity(`Server shutdown`, { type: "WATCHING" });
-                client.user.setPresence({ status: "dnd" })
+                client.user.setPresence({ status: "dnd" });
                 return console.log(`ERROR! SERVER NOT FOUND - SERVER NOT FOUND - SERVER NOT FOUND - SERVER NOT FOUND`.red, `IP: ${cfg.IP}:${cfg.PORT} check he is online?!`.red);
-            }
+            };
             let online = `${data[0]}`.green;
             let max = `${data[1]}`.magenta;
             if (online && max) {
@@ -61,29 +83,33 @@ ports.forEach((port, i) => {
                 } else {
                     online = '0'.red;
                     client.user.setActivity(`Server is empty`, { type: "WATCHING" });
-                }
+                };
             } else
-                client.user.setActivity(`Server not found`)
-            console.log(`[${port}]`.green + ` Online: ${online}/${max}`.cyan);
-            }
-        }, 2000)
-    })
+                client.user.setActivity(`Server not found`);
+
+            if(!data[2]['Players']) client.user.setActivity(`Server not found`); else console.log(`[${port}]`.green + ` Online: ${online}/${max}`.cyan);
+            };
+        }, 5000);
+    });
 
     client.on('message', async message => {
         if (message.content.startsWith(`${cfg.prefix}info`) && message.content.includes(client.user.id)) {
             let data = JSON.parse(fs.readFileSync('./cache.json'));
             let emb = new MessageEmbed()
                 .setTitle(`Server Info`)
-                .addField('IP:', `${cfg.IP}:${port}`)
-                .addField('Players Count:', `${data[port].Players}`)
-                .addField('WhiteList:', data[port].WL)
-                .addField('Frendly Fire:', data[port].FF)
-                .addField('Modded:', data[port].Modded)
-                .addField('Version:', data[port].Version)
-            message.channel.send(emb)
-        }
+                .addFields(
+                    { name: 'IP', value: `${cfg.IP}:${port}` },
+                    { name: 'Players Count', value: `${data[port].Players}` },
+                    { name: 'WhiteList', value: data[port].WL },
+                    { name: 'Frendly Fire', value: data[port].FF },
+                    { name: 'Modded', value: data[port].Modded },
+                    { name: 'Version', value: data[port].Version }
+                )
+                
+            message.channel.send(emb);
+        };
     });
-})
+});
 
 function getonline() {
     return new Promise((resolve, reject) => {
@@ -91,21 +117,11 @@ function getonline() {
             out = out.Servers;
             out.forEach(server => {
                 servers[server.Port] = server;
-            })
-            fs.writeFileSync('./cache.json', JSON.stringify(servers))
-
-            
-            if (out[0])
-                resolve([parseInt(out[0].Players.split("/")[0]), parseInt(out[0].Players.split("/")[1]), out[0].WL, out[0].FF, out[0].Modded, out[0].Version])
-            else resolve('ERROR')
+            });
+            fs.writeFileSync('./cache.json', JSON.stringify(servers));
+            if (out[0]) resolve([parseInt(out[0].Players), out[0].WL, out[0].FF, out[0].Modded, out[0].Version]);else resolve('ERROR');
         });
     });
-}
+};
 
-setInterval(async () => {
-    await getonline();
-}, 15000)
-
-// other
-function timeConverter(UNIX_timestamp) {let a = new Date(UNIX_timestamp); let months = ['01', '02', '03', '04', '05', '06', '07', '08', '09', '10', '11', '12']; let year = a.getFullYear(); let month = months[a.getMonth()]; let date = a.getDate(); let hour = a.getHours(); let min = a.getMinutes();if (date.length == 1)date = `0${date}`;let time = year + ':' + month + ':' + date + ':' + hour + ':' + min;return time;
-}
+setInterval(async()=>{await getonline();}, 18000);
